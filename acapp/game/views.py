08f341a676708699ea543cbc062eeeaed21636d9
kurtknopfler <callmezz@126.com>
2021-12-11 10:40:17 +0800

    finish first page

--- acapp/game/views.py
+++ acapp/game/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">我的第一个网页</h1>'
+    line4 = '<a href="/play/">进入游戏界面</a>'
+    line3 = '<hr>'
+    line2 = '<img src="https://img2.baidu.com/it/u=4106346850,321390637&fm=26&fmt=auto" width=1880>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+def play(request):
+    line2 = '<img src="https://img1.baidu.com/it/u=903227286,1654182403&fm=26&fmt=auto" width=1880>'
+    line3 = '<hr>'
+    line4 = '<a href="/">返回主页</a>'
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    return HttpResponse(line1 + line4 + line3 + line2)
